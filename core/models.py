5958b7e93f2f54d8c2a4f2b3e2edd9ebc2e9c4c8
Dennis Cohn <denniscm20@gmail.com>
2013-07-15 00:18:49 -0500

    Core model updated: Verbose name and help text values added

--- core/models.py
+++ core/models.py
@@ -3,64 +3,100 @@ from django.contrib.auth.models import User, Group
 from django.utils.translation import ugettext as _
 
 class Auditable (models.Model):
-    created_on = models.DateTimeField(auto_now_add=True)
-    modified_on = models.DateTimeField(auto_now_add=True, auto_now=True)
-    created_by = models.ForeignKey(User, related_name='creations')
-    modified_by = models.ForeignKey(User, related_name='modifications')
+    """
+    Abstract class
+    Contains the basic information for auditing an object
+    """
+    created_on = models.DateTimeField(auto_now_add=True, editable=False, verbose_name=_('Created on'))
+    modified_on = models.DateTimeField(auto_now_add=True, auto_now=True, editable=False, verbose_name=_('Modified on'))
+    created_by = models.ForeignKey(User, related_name='creations', editable=False, verbose_name=_('Created by'))
+    modified_by = models.ForeignKey(User, related_name='modifications', editable=False, verbose_name=_('Modified by'))
 
     class Meta:
         abstract = True
 
+
 class Puzzle (models.Model):
-    name = models.CharField(max_length=120)
-    hostname = models.CharField(max_length=250)
+    name = models.CharField(max_length=120, verbose_name=_('Name'), unique=True)
+    hostname = models.CharField(max_length=250, verbose_name=_('Hostname'))
     use_blacklist = models.BooleanField(default=True, help_text=_('Indicates if the list will be a blacklist or a whitelist'))
 
+
 class IP (models.Model):
-    puzzle = models.ForeignKey(Puzzle, related_name='ip_list')
-    ip = models.GenericIPAddressField(protocol='both')
+    puzzle = models.ForeignKey(Puzzle, related_name='ip_list', editable=False)
+    ip = models.GenericIPAddressField(protocol='both', verbose_name=_('IP Address'))
+
+    class Meta:
+        order_with_respect_to = 'puzzle'
+
 
 class DNS (models.Model):
-    ip = models.GenericIPAddressField(protocol='both')
-    description = models.TextField()
-    puzzle = models.ForeignKey(Puzzle, editable=False)
+    ip = models.GenericIPAddressField(protocol='both', verbose_name=_('IP Address'))
+    description = models.CharField(max_length=150, verbose_name=_('Description'))
+    puzzle = models.ForeignKey(Puzzle, editable=False, related_name=_('dns_list'))
+
+    class Meta:
+        order_with_respect_to = 'puzzle'
+
 
 class Route (models.Model):
     puzzle = models.ForeignKey(Puzzle, editable=False)
-    ip = models.GenericIPAddressField(protocol='both')
-    netmask = models.GenericIPAddressField(protocol='both', default='255.255.255.0')
-    gateway = models.GenericIPAddressField(protocol='both')
-    device = models.ForeignKey(Interface)
-    default = models.GenericIPAddressField(protocol='both')
+    ip = models.GenericIPAddressField(protocol='both', verbose_name=_('IP Address'))
+    netmask = models.GenericIPAddressField(protocol='both', default='255.255.255.0', verbose_name=_('Netmask'))
+    gateway = models.GenericIPAddressField(protocol='both', verbose_name=_('Gateway'))
+    device = models.ForeignKey(Interface, verbose_name=_('Network Interface'))
+    default = models.BooleanField(verbose_name=_('Is default gateway'))
+
+    class Meta:
+        order_with_respect_to = 'puzzle'
+
 
 class Interface (models.Model):
     puzzle = models.ForeignKey(Puzzle, editable=False)
-    device_name = models.CharField(max_length=10, editable=False)
-    ip = models.GenericIPAddressField(protocol='both')
-    netmask = models.GenericIPAddressField(protocol='both')
-    is_wlan = models.BooleanField()
-    mac_address = models.CharField(editable=False)
-
-class Network (models.Model):
-    interface = models.ForeignKey(Interface)
-    name = models.CharField(max_length=120)
-    ip = models.GenericIPAddressField(protocol='both')
-    netmask = models.GenericIPAddressField(protocol='both')
-    shortmask = models.PositiveIntegerField()
-
-class Node (models.Model):
-    hostname = models.CharField(max_length=250, db_index=True, unique=True)
-    network = models.ForeignKey(Network)
-    ip = models.GenericIPAddressField(protocol='both')
-
-class Module (models.Model):
-    puzzle = models.ForeignKey(Puzzle)
-    name = models.CharField(max_length=120, unique=True)
-    is_enabled = models.BoleanField(default=False)
-    dependencies = models.ManyToManyField('self')
+    device_name = models.CharField(max_length=8, editable=False, verbose_name=_('Device Name'))
+    ip = models.GenericIPAddressField(protocol='both', verbose_name=_('IP Address'))
+    netmask = models.GenericIPAddressField(protocol='both', verbose_name=_('Netmask'))
+    is_wlan = models.BooleanField(verbose_name=_('Is WLAN'), help_text=_('Is the interface connected to the internet'))
+    mac_address = models.CharField(editable=False, verbose_name=_('Hardware Address'))
 
     class Meta:
+        order_with_respect_to = 'puzzle'
+
+
+class Network (models.Model):
+    """
+    Class that represents a Net / Subnet
+    """
+    interface = models.ForeignKey(Interface, verbose_name=_('Network Interface'))
+    name = models.CharField(max_length=120, verbose_name=_('Name'), help_text=_('Name that describes the net/subnet'))
+    ip = models.GenericIPAddressField(protocol='both', verbose_name=_('IP Address'))
+    netmask = models.GenericIPAddressField(protocol='both', verbose_name=_('Netmask'))
+    vlsm = models.PositiveIntegerField(verbose_name=_('VLSM'), help_text=_('Variable Length Subnet Mask'))
+
+    class Meta:
+        order_with_respect_to = 'puzzle'
+
+
+class Node (models.Model):
+    fqdn = models.CharField(max_length=250, db_index=True, unique=True, verbose_name=_('FQDN'))
+    network = models.ForeignKey(Network, verbose_name=_('Network'), help_text=_('Network the node belogns'))
+    ip = models.GenericIPAddressField(protocol='both', blank=True, null=True, verbose_name=_('IP Address'), help_text=_('Leave blank if you want to use the fqdn'))
+
+    class Meta:
+        order_with_respect_to = 'puzzle'
+
+
+class Module (models.Model):
+    puzzle = models.ForeignKey(Puzzle, editable=False)
+    name = models.CharField(max_length=120, unique=True, verbose_name=_('Name'))
+    is_enabled = models.BoleanField(default=False, verbose_name=_('Is enabled'))
+    dependencies = models.ManyToManyField('self', verbose_name=_('Dependencies'))
+    installed_path = models.FilePathField(verbose_name=_('Installed Path'))
+
+    class Meta:
+        order_with_respect_to = 'puzzle'
         ordering = ['is_enabled', 'name']
+
 
 class LogStatus (object):
     SUCCESS = 0
@@ -81,7 +117,7 @@ class Protocol(object):
     UDP = 2
     ICMP = 3
     IP = 4
-   
+
     CHOICES = (
         (ANY, 'any'),
         (TCP, 'tcp'),
@@ -90,21 +126,23 @@ class Protocol(object):
         (IP, 'ip'),
     )
 
+
 class Service (models.Model):
-    port = model.PositiveIntegerField()
-    protocol = model.SmallPositiveIntegetField(choices=Protocol.CHOICES)
-    name = model.CharField(max_length=20)
+    port = model.PositiveIntegerField(verbose_name=_('Port'))
+    protocol = model.SmallPositiveIntegetField(choices=Protocol.CHOICES, verbose_name=_('Protocol'))
+    name = model.CharField(max_length=20, verbose_name=_('Name'))
 
     class Meta:
         unique_together = [('port', 'protocol')]
         ordering = ['port', 'protocol']
 
+
 class Log (models.Model):
-    datetime_registered = models.DateTimeField(auto_now_add=True, editable=False, db_index=True)
-    status = models.PositiveSmallIntergerField(choices=LogStatus.CHOICES, editable=False, db_index=True)
-    description = models.TextField(editable=False)
-    url = models.URLField(editable=False)
-    user = models.ForeignKey(User, null=True, blank=True, editable=False)
+    datetime_registered = models.DateTimeField(auto_now_add=True, editable=False, db_index=True, verbose_name=_('Datetime'))
+    status = models.PositiveSmallIntergerField(choices=LogStatus.CHOICES, editable=False, db_index=True, verbose_name=_('Status'))
+    description = models.TextField(editable=False, verbose_name=_('Description'))
+    url = models.URLField(editable=False, verbose_name=_('URL'))
+    user = models.ForeignKey(User, null=True, blank=True, editable=False, verbose_name=_('User'), related_name='events')
 
     class Meta:
         ordering = ['-datetime']
